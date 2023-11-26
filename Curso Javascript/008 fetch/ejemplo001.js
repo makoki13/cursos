303fca3265e5e943b440a5a05dabed57623106b9
makoki13 <pablo.makoki@gmail.com>
2023-11-26 10:36:56 +0100

    curso en pdf

--- Curso Javascript/008 fetch/ejemplo001.js
+++ Curso Javascript/008 fetch/ejemplo001.js
@@ -1,31 +1,21 @@
-const url = 'https://jsonplaceholder.typicode.com/users';
-
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const ul = document.createElement('ul');
-        data.forEach(user => {
-            const li = document.createElement('li');
-            li.textContent = user.name;
-            ul.appendChild(li);
-        });
-        document.body.appendChild(ul);
+function carga() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Solicitud fallida');
+      }
+      return response.json(); // Analiza la respuesta como JSON
     })
-    .catch(error => console.error(error));
-
-
-/* Con control de errores */
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Solicitud fallida');
-    }
-    return response.json(); // Analiza la respuesta como JSON
-  })
-  .then(users => {
-    console.log('Lista de usuarios:');
-    console.log(users);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    .then(users => {
+      const ul = document.createElement('ul');
+      users.forEach(user => {
+        const li = document.createElement('li');
+        li.textContent = user.name;
+        ul.appendChild(li);
+      });
+      document.body.appendChild(ul);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
